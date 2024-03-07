9e7ca8d0bdb8d3ee75b4cd912508b585890a9d66
Byzthr <byzthr@outlook.com>
2024-03-07 14:00:57 +0100

    Config tweaks

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangofirststeps.urls'
+ROOT_URLCONF = 'firstsite.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangofirststeps.wsgi.application'
+WSGI_APPLICATION = 'firstsite.wsgi.application'
 
 
 # Database
