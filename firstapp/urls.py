295e04227606c9dde9af6307f8943cb856427a45
Byzthr <byzthr@outlook.com>
2024-04-17 12:23:28 +0200

    admin and other model functionalities

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.hello),
     path('index/', views.index),
     path('number/<int:number>', views.number),
     path('hello/<str:username>', views.hello),
-    path('about/', views.about)
+    path('about/', views.about),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks),
+    path('task/<int:id>', views.task),
+    path('myprojects/', views.myprojects),
+    path('mytasks/', views.mytasks),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Project, Task
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse(f"<h1>Main Page<h1/>")
 
 def hello(request, username):
     return HttpResponse(f"<h1>Hello {username}<h1/>")
@@ -12,5 +10,27 @@ def hello(request, username):
 def number(request, number):
     return HttpResponse(f"<h1>Your number: {number}<h1/>")
 
+def index(request):
+    title = "Welcome to my first page"
+    return render(request, 'index.html', {'title':title})
+
 def about(request):
-    return HttpResponse("<h1>About page<h1/>")
+    return render(request, 'about.html')
+
+def projects(request):
+    return JsonResponse(list(Project.objects.values()), safe=False)
+
+def tasks(request):
+    return JsonResponse(list(Task.objects.values()), safe=False)
+
+def task(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f"Task {id}: {task.title}")
+
+def myprojects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {'projects':projects})
+
+def mytasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks':tasks})
